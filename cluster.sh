1021a138d7a5bfd4b065e31646d48532858d1cbe
el(Jiung Jeong) <astral1@coupang.com>
2015-12-31 10:44:50 +0900

    complete basic functions

--- cluster.sh
+++ cluster.sh
@@ -2,13 +2,21 @@
 
 . `dirname $0`/templates.tpl
 
+CONF_ROOT=$HOME/.coreos-utils
+
 function usage {
   local PROG=`basename $0`
-  echo "$PROG:"
-  echo "  $PROG new <cluster name> [<cluster initial size>]: create new cluster. default initial size is 1."
-  echo "  $PROG run <cluster name> [<cluster initial size>]: run new cluster. default initial size is 2."
-  echo "  $PROG del <cluster name>: delete new cluster."
-  echo "  $PROG clean <cluster name>: remove all members from cluster."
+  echo "USAGE for $PROG:"
+  echo "  $PROG new <cluster name> [<cluster initial size>]:
+      create new cluster. default initial size is 1."
+  echo "  $PROG run <cluster name> [<cluster size>]:
+      run new cluster. default size is 1. if cluster size is less than initial size, it will be modified to initial size."
+  echo "  $PROG stop <cluster name>:
+      stop running cluster. kill all hosts"
+  echo "  $PROG del <cluster name>:
+      delete new cluster."
+  echo "  $PROG clean <cluster name>:
+      remove all members from cluster."
 }
 
 function new {
@@ -17,6 +25,7 @@ function new {
   local CLUSTER=`echo -n $NAME | shasum | awk '{print $1}'`
 
   _config_etcd
+  _config_cloud $NAME
 
   etcdctl ls discovery/$CLUSTER &> /dev/null
   local EXIST=$?
@@ -29,12 +38,15 @@ function new {
 function run {
   local NAME=$1
   local SIZE=${2:-1}
-  local CONF=`_config_cloud $NAME`
+  local CLUSTER=`echo -n $NAME | shasum | awk '{print $1}'`
+  local CONF=`_cloud_config_path $NAME`
 
   _config_etcd
 
-  local INIT_SIZE=`etcdctl get discovery/$CLUSTER/_config/size`
+  local INIT_SIZE=`etcdctl get discovery/$CLUSTER/_config/size 2> /dev/null`
   [[ $SIZE -lt $INIT_SIZE ]] && SIZE=$INIT_SIZE
+
+  cd $CONF_ROOT
 
   if [ $SIZE -eq 1 ]; then
     sudo corectl run --channel beta --cloud_config $CONF --name $NAME -d
@@ -45,11 +57,21 @@ function run {
   fi
 }
 
+function stop {
+  local NAME=$1
+
+  for NODE in `corectl query -a | sed '1d' | grep test\[0-9\]\* | awk '{print $6}'`; do
+    corectl kill $NODE
+  done
+
+  clean $NAME
+}
+
 function del {
   local NAME=$1
   local CLUSTER=`echo -n $NAME | shasum | awk '{print $1}'`
 
-  _config_etcd
+  stop $NAME
 
   etcdctl ls discovery/$CLUSTER &> /dev/null
   local EXIST=$?
@@ -59,6 +81,8 @@ function del {
     etcdctl rmdir discovery/$CLUSTER/_config &> /dev/null
     etcdctl rmdir discovery/$CLUSTER &> /dev/null
   fi
+
+  rm -f $HOME/.coreos-utils/$NAME-init.yml
 }
 
 function clean {
@@ -97,20 +121,23 @@ EOF
 
 function _config_cloud {
   local NAME=$1
-  local CONFPATH=$HOME/.coreos-utils
-  [[ -d $CONFPATH ]] || mkdir -p $CONFPATH
-  local CONF=$CONFPATH/$NAME-init.yml
 
-  cat <<EOF > $CONF
+  cat <<EOF > $CONF_ROOT/`_cloud_config_path $NAME`
 $(tpl_cloud $NAME)
 EOF
-  echo -n $CONF
+}
+
+function _cloud_config_path {
+  local NAME=$1
+
+  [[ -d $CONF_ROOT ]] || mkdir -p $CONF_ROOT
+
+  echo -n $NAME-init.yml
 }
 
 COMMAND=$1
 shift
 
-tpl_cloud
 case $COMMAND in
   new)
     `dirname $0`/prereq.sh
@@ -119,6 +146,10 @@ case $COMMAND in
   run)
     `dirname $0`/prereq.sh
     run $@
+    ;;
+  stop)
+    `dirname $0`/prereq.sh
+    stop $@
     ;;
   del)
     `dirname $0`/prereq.sh
