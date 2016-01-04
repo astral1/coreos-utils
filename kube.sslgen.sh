d42670f40ecce41a9e8e3f92dddcade34045ba26
el(Jiung Jeong) <astral1@coupang.com>
2016-01-04 15:50:26 +0900

    fix cert path

--- kube.sslgen.sh
+++ kube.sslgen.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+cd `dirname $0`
+mkdir -p cert
+cd cert
+
 openssl genrsa -out ca-key.pem 2048
 openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=kube-ca"
 
