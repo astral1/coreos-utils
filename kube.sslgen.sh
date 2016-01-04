dedf577857389f0e1a39dcf867635114d734ec20
el(Jiung Jeong) <astral1@coupang.com>
2016-01-04 15:46:59 +0900

    add ssl keygen script(not parameterized)

---
+++ kube.sslgen.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+openssl genrsa -out ca-key.pem 2048
+openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj "/CN=kube-ca"
+
+K8S_SERVICE_IP=192.168.64.40
+MASTER_HOST=192.168.64.40
+
+cat <<EOF > openssl.cnf
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = kubernetes
+DNS.2 = kubernetes.default
+IP.1 = ${K8S_SERVICE_IP}
+IP.2 = ${MASTER_HOST}
+EOF
+
+openssl genrsa -out apiserver-key.pem 2048
+openssl req -new -key apiserver-key.pem -out apiserver.csr -subj "/CN=kube-apiserver" -config openssl.cnf
+openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile openssl.cnf
+
+cat <<EOF > worker-openssl.cnf
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[req_distinguished_name]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+IP.1 = \$ENV::WORKER_IP
+EOF
+
+for CONF in `echo coreos01:192.168.64.40 coreos02:192.168.64.41 coreos03:192.168.64.42`; do
+  WORKER_FQDN=`echo $CONF | cut -d: -f 1`
+  WORKER_IP=`echo $CONF | cut -d: -f 2`
+  openssl genrsa -out ${WORKER_FQDN}-worker-key.pem 2048
+  WORKER_IP=${WORKER_IP} openssl req -new -key ${WORKER_FQDN}-worker-key.pem -out ${WORKER_FQDN}-worker.csr -subj "/CN=${WORKER_FQDN}" -config worker-openssl.cnf
+  WORKER_IP=${WORKER_IP} openssl x509 -req -in ${WORKER_FQDN}-worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out ${WORKER_FQDN}-worker.pem -days 365 -extensions v3_req -extfile worker-openssl.cnf
+done
+
+openssl genrsa -out admin-key.pem 2048
+openssl req -new -key admin-key.pem -out admin.csr -subj "/CN=kube-admin"
+openssl x509 -req -in admin.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out admin.pem -days 365
